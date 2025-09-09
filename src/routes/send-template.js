b3044ab1fed9e73fa1db82570db6ec181cc5ad9e
Anmol452 <anmolsingh26ff@gmail.com>
2025-09-09 10:00:39 +0530

    first klkllhihdfljffkljkjc

--- src/routes/send-template.js
+++ src/routes/send-template.js
@@ -6,25 +6,57 @@ const router = express.Router();
 
 
 router.post("/", async (req, res) => {
-     try {
+    try {
     const response = await fetch(
       `https://graph.facebook.com/v21.0/${process.env.PHONE_NUMBER_ID}/messages`,
       {
         method: "POST",
         headers: {
-          "Authorization": `Bearer ${process.env.WHATSAPP_TOKEN}`,
-          "Content-Type": "application/json"
+          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
+          "Content-Type": "application/json",
         },
         body: JSON.stringify({
           messaging_product: "whatsapp",
-          to: req.body.to,  // "91XXXXXXXXXX"
+          recipient_type: "individual",
+          to: req.body.to, // e.g. "919876543210"
           type: "template",
           template: {
-            name: req.body.template_name, // "order_update"
-            language: { code: req.body.language_code || "en_US" },
-            components: req.body.components || [] // optional parameters
+            name: "testing", // 👈 Meta में approved template name
+            language: { code: "en_US" },
+            components: [
+              {
+                type: "body",
+                parameters: [
+                  {
+                    type: "text",
+                    text: "Hello Anmol 👋"
+                  },
+                  {
+                    type: "currency",
+                    currency: {
+                      fallback_value: "$100.99",
+                      code: "USD",
+                      amount_1000: 100990
+                    }
+                  },
+                  {
+                    type: "date_time",
+                    date_time: {
+                      fallback_value: "February 25, 1977",
+                      day_of_week: 5,
+                      year: 1977,
+                      month: 2,
+                      day_of_month: 25,
+                      hour: 15,
+                      minute: 33,
+                      calendar: "GREGORIAN"
+                    }
+                  }
+                ]
+              }
+            ]
           }
-        })
+        }),
       }
     );
 
